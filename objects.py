265981042c72d858ced7fc454e851e7898106c73
BenjaminHuangPA <ben@aviatrix.com>
2019-10-25 00:33:01 -0400

    changes 10/25/2019

---
+++ objects.py
@@ -0,0 +1,33 @@
+
+import pygame
+from assets import *
+
+pygame.display.init()
+
+
+
+
+cave_plant1 = Prop(450, 80, "Cave plant", 20, False, "cave_plant.png")
+
+cave_plant2 = Prop(120, 120, "Cave plant", 20, False, "cave_plant.png")
+
+cave_plant3 = Prop(158, 194, "Cave plant", 20, False, "cave_plant.png")
+
+cave_plant4 = Prop(246, 327, "Cave plant", 20, False, "cave_plant.png")
+
+cave_plant5 = Prop(104, 91, "Cave plant", 20, False, "cave_plant.png")
+
+cave_plant6 = Prop(202, 404, "Cave plant", 20, False, "cave_plant.png")
+
+cave_plant7 = Prop(334, 180, "Cave plant", 20, False, "cave_plant.png")
+
+cave_plant8 = Prop(94, 422, "cave plant", 20, False, "cave_plant.png")
+
+props_z1r1 = [cave_plant1, cave_plant2]
+
+props_z1r2 = [cave_plant3, cave_plant4]
+
+props_z1r3 = [cave_plant5, cave_plant6]
+
+props_z1r4 = [cave_plant7, cave_plant8]
+
