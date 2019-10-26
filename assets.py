6bc9f87cd9c302ef087278b4b12f1fb5d90bfd83
BenjaminHuangPA <ben@aviatrix.com>
2019-10-26 01:14:17 -0400

    10/25/2019

--- assets.py
+++ assets.py
@@ -99,6 +99,26 @@ class Player(pygame.sprite.Sprite):
 
     def heal(self, HP):
         self.HP = self.HP + HP
+
+    def equipArmor(self, armor):
+        if armor.region == "head":
+            self.headArmor = armor
+        elif armor.region == "torso":
+            self.torsoArmor = armor
+        elif armor.region == "arms":
+            self.armArmor = armor
+        elif armor.region == "legs":
+            self.legArmor = armor
+
+    def unequipArmor(self, region):
+        if region == "head":
+            self.headArmor = None
+        elif region == "torso":
+            self.torsoArmor = None
+        elif region == "arms":
+            self.armArmor = None
+        elif region == "legs":
+            self.legArmor = None
         
     def walkAnim(self, x, y):
         if x < 0: #if the player walks to the left (change in x pos less than 0)
@@ -245,16 +265,24 @@ class spriteSheet(object): #create spriteSheet class to handle sprite sheets
         surface.blit(self.sheet, (x, y, self.cellWidth, self.cellHeight))
 
 class Armor(pygame.sprite.Sprite):
-    def __init__(self, game, name, x, y, forwardSpriteSheetName, backwardSpriteSheetName, leftSpriteSheetName, rightSpriteSheetName, itemImage, equipImage):
+    def __init__(self, game, name, x, y, region, forwardSpriteSheetName, backwardSpriteSheetName, leftSpriteSheetName, rightSpriteSheetName, itemImage, equipImage, value, description):
         self.groups = game.all_sprites
 
         pygame.sprite.Sprite.__init__(self, self.groups)
 
         self.game = game
 
+        self.name = name
+
         self.x = x
 
         self.y = y
+
+        self.region = region
+
+        #TODO: implement description attribute
+
+        self.type = "armor"
 
         self.image = pygame.image.load(itemImage).convert_alpha()
 
@@ -272,7 +300,11 @@ class Armor(pygame.sprite.Sprite):
 
         self.rightSpriteSheetName = rightSpriteSheetName
 
-        self.equipImage = equipImage
+        self.equipImage = pygame.image.load(equipImage).convert_alpha()
+
+        self.value = value
+
+        self.description = description
 
         self.forwardSpriteSheet = None #remember: these are LISTS OF COORDINATES CORRESPONDING TO A SPRITE SHEET, NOT THE SPRITE SHEET ITSELF!!!!
 
@@ -414,6 +446,8 @@ class item(pygame.sprite.Sprite):
         self.x = x
 
         self.y = y
+
+        self.type = "item"
 
         self.image = pygame.image.load(image).convert()
 
@@ -559,6 +593,65 @@ class statusBar(pygame.sprite.Sprite):
         self.image.blit(dexterity, (273, 87))
         self.image.blit(guard, (274, 107))
 
+
+class textWrapper(object):
+
+    #The purpose of this class is thus: Pygame does not natively implement a way
+    #to write text on multiple lines, and new line characters are ignored. Thus,
+    #I wrote this class to format text that would be too long to fit a given width
+    #into a paragraph.
+
+    
+    def __init__(self):
+        pass #no need to pass anything into the constructor
+
+    def blitText(self, surface, x, y, text, width, font_size, line_height, color):
+        R = color[0]
+
+        G = color[1] #pass in the color as an RGB tuple
+
+        B = color[2]
+
+        pygame.font.init() #initialize pygame's font module
+
+        textArray = text.split() #split the inputted text into separate words
+
+        font = pygame.font.SysFont("Times New Roman", font_size) #initialize font object using the font size module
+
+        yCoord = y #this variable will store the y coordinate of the lines we draw the text on.
+
+        currentLineLength = 0 #this variable will store the length of the current line of text, for checking if we've exceeded the target width.
+
+        textToDraw = "" #this variable (initialized to an empty String) will store the text to be written for each line.
+
+        textSize = font.size(text) #this method returns a tuple of its argument's dimensions in the form (width, height)
+        if textSize[0] < width:
+            #check if the text passed in is no more than a single line's worth. If that's the case, we can just blit it directly to the surface without having to worry about
+            #new lines
+            line = font.render(text, False, (R, G, B))
+            surface.blit(line, (x, yCoord))
+        else:
+            #now we need to handle if the text passed in has a length greater than a single line
+            for word in textArray:
+                wordSize = font.size(word) #get the tuple describing each word's dimensions
+                wordLength = wordSize[0] + 15 #get the word's length. Add +15 pixels to account for spaces.
+                
+                if currentLineLength + wordLength < width:
+                    currentLineLength += wordLength
+                    textToDraw = textToDraw + word + " "
+                    #if we can add this word to the current line of text to be drawn (textToDraw) without exceeding the width of a single line, do so
+                elif currentLineLength + wordLength > width:
+                    #else, if adding the word would go over the limit imposed:
+                    line = font.render(textToDraw, False, (R, G, B)) #render the text onto a surface
+                    surface.blit(line, (x, yCoord)) #draw the surface containing the text onto the surface passed in
+                    yCoord += line_height #increment yCoord to go to a new line.
+                    currentLineLength = wordLength  #add the word's length to the currentLineLength variabel
+                    textToDraw = word + " " #move the word that would have caused the line of text to go over the width to the start of the new line.
+       
+        lastLine = font.render(textToDraw, False, (R, G, B)) #finally, blit the last line of text onto the screen (which is almost always shorter than width and as a result is
+        #not drawn by the above if statement
+        surface.blit(lastLine, (x, yCoord))
+            
         
             
         
@@ -605,6 +698,8 @@ class inventoryGUI(pygame.sprite.Sprite): #inventory GUI class
 
         self.previousSelection = 0 #initialize variable to hold the previously selected item (for blitting purposes)
 
+        self.currentEquipmentSelection = None
+
         self.selectionImage = pygame.image.load("selected.png").convert_alpha() #load the image to show that an item is selected (red border)
 
         self.deselectionImage = pygame.image.load("deselected.png").convert_alpha() #load the image to show that an item has been "deselected" (plain background with no red border)
@@ -638,15 +733,17 @@ class inventoryGUI(pygame.sprite.Sprite): #inventory GUI class
         self.currentSelection = item #set the currentSelection variable to the newly selected image
         if self.previousSelection != 0:
             self.deselect(self.previousSelection) #don't deselect the previous item if there was no previously deselected item
-        print(self.currentSelection.name)
-        print(self.currentSelection.value)
-        print(self.currentSelection.description)
+        
 
     def deselect(self, item):
         self.image.blit(self.deselectionImage, (self.previousSelection.x, self.previousSelection.y)) #for the previously selected item, blit the plain grey background (erases the red one)
         self.image.blit(item.image, (item.x, item.y)) #reblit the previously selected item's image
 
     def inspect(self): #308, 22
+
+
+        testTextWrapper = textWrapper()
+        
         #this method is run as a result of clicking the "inspect" button
         if(self.currentSelection == 0):
             print("Cannot inspect nothing!") #make sure I'm not accessing a thing that isn't of the item class
@@ -659,15 +756,85 @@ class inventoryGUI(pygame.sprite.Sprite): #inventory GUI class
             name = font1.render(self.currentSelection.name, False, (255, 255, 255)) #create name text for the currently selected object to inspect
             desc = font1.render(self.currentSelection.description, False, (255, 255, 255)) #create description text for the currently selected object to inspect
             #value = font1.render(self.currentSelection.value, False, (255, 255, 255))
+            
             inspectionWindow.blit(name, (56, 161)) #blit name/desc text to the inspectionWindow image
-            inspectionWindow.blit(desc, (79, 194))
 
-            scaledImage = pygame.transform.scale(self.currentSelection.image, (128, 128))
+            #inspectionWindow.blit(desc, (24, 205))
+
+            testTextWrapper.blitText(inspectionWindow, 24, 205, self.currentSelection.description, 200, 9, 11, (198, 187, 187))
+
+            if(self.currentSelection.type == "armor"):
+                scaledImage = pygame.transform.scale(self.currentSelection.equipImage, (128, 128))
+            else:
+                scaledImage = pygame.transform.scale(self.currentSelection.image, (128, 128))
             inspectionWindow.blit(scaledImage, (24, 24))
             
             #inspectionWindow.blit(value, (98, 256))
             self.image.blit(inspectionWindow, (308, 22)) #blit inspectionWindow image to screen
         
+    def equip(self):
+        if self.currentSelection.type != "armor" and self.currentSelection.type != "weapon":
+            print("Cannot equip that item!")
+        else:
+
+            if self.currentSelection.type == "armor":
+                if self.currentSelection.region == "head":
+                    self.image.blit(self.currentSelection.equipImage, (406, 18))
+                elif self.currentSelection.region == "torso":
+                    self.image.blit(self.currentSelection.equipImage, (406, 100))
+                elif self.currentSelection.region == "arms":
+                    self.image.blit(self.currentSelection.equipImage, (406, 182))
+                elif self.currentSelection.region == "legs":
+                    self.image.blit(self.currentSelection.equipImage, (407, 864))
+
+                self.player.equipArmor(self.currentSelection)
+
+    def selectEquipment(self, slot):
+        equipmentSelected = pygame.image.load("equipment_selected.png").convert_alpha()
+        if slot == "head" and self.player.headArmor != None:
+            self.image.blit(equipmentSelected, (406, 18))
+            self.currentEquipmentSelection = "head"
+        elif slot == "torso" and self.player.torsoArmor != None:
+            self.image.blit(equipmentSelected, (406, 200))
+            self.currentEquipmentSelection = "torso"
+        elif slot == "arms" and self.player.armArmor != None:
+            self.image.blit(equipmentSelected, (406, 182))
+            self.currentEquipmentSelection = "arms"
+        elif slot == "legs" and self.player.legArmor != None:
+            self.image.blit(equipmentSelected, (406, 164))
+            self.currentEquipmentSelection = "legs"
+    
+            
+        self.equipButton.image = pygame.image.load("unequip_button.png").convert_alpha()
+
+        self.initializeButtons()
+
+       
+
+    def unequip(self):
+
+        equipmentUnselected = pygame.image.load("nothing_equipped.png").convert_alpha()
+        
+        if self.currentEquipmentSelection == "head":
+            self.player.unequipArmor("head")
+            self.image.blit(equipmentUnselected, (406, 18))
+        elif self.currentEquipmentSelection == "torso":
+            self.player.unequipArmor("torso")
+            self.image.blit(equipmentUnselected, (406, 200))
+        elif self.currentEquipmentSelection == "arms":
+            self.player.unequipArmor("arms")
+            self.image.blit(equipmentUnselected, (406, 182))
+        elif self.currentEquipmentSelection == "legs":
+            self.player.unequipArmor("legs")
+            self.image.blit(equipmentUnselected, (406, 164))
+
+        self.equipButton.image = pygame.image.load("equip_button.png").convert_alpha()
+
+        self.initializeButtons()
+
+        self.currentEquipmentSelection = None
+
+
         
                     
                 
